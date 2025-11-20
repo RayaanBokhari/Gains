50f60306ee346abae9b059f94f1c5fa02dec3c31
Rayaan Bokhari <rayaan.bokhari@gmail.com>
2025-11-20 16:43:14 -0500

    Initial Commit

---
+++ GainsTests/GainsTests.swift
@@ -0,0 +1,17 @@
+//
+//  GainsTests.swift
+//  GainsTests
+//
+//  Created by Rayaan Bokhari on 11/20/25.
+//
+
+import Testing
+@testable import Gains
+
+struct GainsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
