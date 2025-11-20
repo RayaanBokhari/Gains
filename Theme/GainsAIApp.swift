720a5cf260b3dec120abb51b6afc70d9318d39b6
Rayaan Bokhari <rayaan.bokhari@gmail.com>
2025-11-20 16:54:27 -0500

    Initial commit

---
+++ Theme/GainsAIApp.swift
@@ -0,0 +1,16 @@
+//
+//  GainsAIApp.swift
+//  Gains
+//  Created by Rayaan Bokhari on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct GainsAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Theme/colors.swift
@@ -0,0 +1,6 @@
+//
+//  Colors.swift
+//  Gains
+//  Created by Rayaan Bokhari on 11/20/25.
+//
+
